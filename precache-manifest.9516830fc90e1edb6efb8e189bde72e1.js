a5cbe22b0a16fd71484276abe636822c48d9db4a
em4michael <aniehegc@gmail.com>
2022-05-31 12:35:52 -0700

    Updates

---
+++ precache-manifest.9516830fc90e1edb6efb8e189bde72e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b20bf5d310bd8873a3f",
+    "url": "/React-Weather-App/static/css/main.7029b408.chunk.css"
+  },
+  {
+    "revision": "7b20bf5d310bd8873a3f",
+    "url": "/React-Weather-App/static/js/main.7b20bf5d.chunk.js"
+  },
+  {
+    "revision": "f993ed0ea40465f97e7e",
+    "url": "/React-Weather-App/static/js/1.f993ed0e.chunk.js"
+  },
+  {
+    "revision": "92be0f919fa8464bebdb",
+    "url": "/React-Weather-App/static/js/2.92be0f91.chunk.js"
+  },
+  {
+    "revision": "fd110c678591086de634",
+    "url": "/React-Weather-App/static/js/runtime~main.fd110c67.js"
+  },
+  {
+    "revision": "432ae5ed706d69eee5de4e97d9aa3842",
+    "url": "/React-Weather-App/static/media/Rain.432ae5ed.png"
+  },
+  {
+    "revision": "9e51f2b09d954cecd83170ab002e2269",
+    "url": "/React-Weather-App/static/media/humidity.9e51f2b0.png"
+  },
+  {
+    "revision": "a0cbbfddd5745582a8b2f112bf6a7db0",
+    "url": "/React-Weather-App/static/media/Sunny.a0cbbfdd.png"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/React-Weather-App/static/media/warm-bg.635ed87d.jpg"
+  },
+  {
+    "revision": "d1f59d52e9ed88d7c2e366efde1fa063",
+    "url": "/React-Weather-App/index.html"
+  }
+];
